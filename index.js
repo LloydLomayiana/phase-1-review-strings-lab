34670636f73850f082983c11ee3499e4127b2da4
LloydLomayiana <Lloydloma@gmail.com>
2022-11-28 09:45:37 +0300

    CC

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Mark Zuck';
+const welcomeMessage = 'Welcome to Flatbook, Mark Zuck!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, MARK ZUCK!';
+const shortGreeting = 'Welcome, M!'
